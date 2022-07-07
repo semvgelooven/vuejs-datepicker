fb8e371b586f24c8b87ab68be3b841686f61608d
Sem van Gelooven <sem@secretninjas.nl>
2022-07-07 14:51:30 +0200

    Fix Date.parse format

--- src/components/DateInput.vue
+++ src/components/DateInput.vue
@@ -1,8 +1,14 @@
 <template>
-  <div :class="{'input-group' : bootstrapStyling}">
+  <div :class="{ 'input-group': bootstrapStyling }">
     <!-- Calendar Button -->
-    <span v-if="calendarButton" class="vdp-datepicker__calendar-button" :class="{'input-group-prepend' : bootstrapStyling}" @click="showCalendar" v-bind:style="{'cursor:not-allowed;' : disabled}">
-      <span :class="{'input-group-text' : bootstrapStyling}">
+    <span
+      v-if="calendarButton"
+      class="vdp-datepicker__calendar-button"
+      :class="{ 'input-group-prepend': bootstrapStyling }"
+      @click="showCalendar"
+      v-bind:style="{ 'cursor:not-allowed;': disabled }"
+    >
+      <span :class="{ 'input-group-text': bootstrapStyling }">
         <i :class="calendarButtonIcon">
           {{ calendarButtonIconContent }}
           <span v-if="!calendarButtonIcon">&hellip;</span>
@@ -26,10 +32,17 @@
       @click="showCalendar"
       @keyup="parseTypedDate"
       @blur="inputBlurred"
-      autocomplete="off">
+      :maxlength="maxLength"
+      autocomplete="off"
+    />
     <!-- Clear Button -->
-    <span v-if="clearButton && selectedDate" class="vdp-datepicker__clear-button" :class="{'input-group-append' : bootstrapStyling}" @click="clearDate()">
-      <span :class="{'input-group-text' : bootstrapStyling}">
+    <span
+      v-if="clearButton && selectedDate"
+      class="vdp-datepicker__clear-button"
+      :class="{ 'input-group-append': bootstrapStyling }"
+      @click="clearDate()"
+    >
+      <span :class="{ 'input-group-text': bootstrapStyling }">
         <i :class="clearButtonIcon">
           <span v-if="!clearButtonIcon">&times;</span>
         </i>
@@ -39,7 +52,7 @@
   </div>
 </template>
 <script>
-import { makeDateUtils } from '../utils/DateUtils'
+import { makeDateUtils } from "../utils/DateUtils";
 export default {
   props: {
     selectedDate: Date,
@@ -62,66 +75,75 @@ export default {
     required: Boolean,
     typeable: Boolean,
     bootstrapStyling: Boolean,
-    useUtc: Boolean
+    useUtc: Boolean,
+    formatTypedDate: Function,
+    maxLength: Number,
   },
-  data () {
-    const constructedDateUtils = makeDateUtils(this.useUtc)
+  data() {
+    const constructedDateUtils = makeDateUtils(this.useUtc);
     return {
       input: null,
       typedDate: false,
-      utils: constructedDateUtils
-    }
+      utils: constructedDateUtils,
+    };
   },
   computed: {
-    formattedValue () {
+    formattedValue() {
       if (!this.selectedDate) {
-        return null
+        return null;
       }
       if (this.typedDate) {
-        return this.typedDate
+        return this.typedDate;
       }
-      return typeof this.format === 'function'
+      return typeof this.format === "function"
         ? this.format(this.selectedDate)
-        : this.utils.formatDate(new Date(this.selectedDate), this.format, this.translation)
+        : this.utils.formatDate(
+            new Date(this.selectedDate),
+            this.format,
+            this.translation
+          );
     },
 
-    computedInputClass () {
+    computedInputClass() {
       if (this.bootstrapStyling) {
-        if (typeof this.inputClass === 'string') {
-          return [this.inputClass, 'form-control'].join(' ')
+        if (typeof this.inputClass === "string") {
+          return [this.inputClass, "form-control"].join(" ");
         }
-        return {'form-control': true, ...this.inputClass}
+        return { "form-control": true, ...this.inputClass };
       }
-      return this.inputClass
-    }
+      return this.inputClass;
+    },
   },
   watch: {
-    resetTypedDate () {
-      this.typedDate = false
-    }
+    resetTypedDate() {
+      this.typedDate = false;
+    },
   },
   methods: {
-    showCalendar () {
-      this.$emit('showCalendar')
+    showCalendar() {
+      this.$emit("showCalendar");
     },
     /**
      * Attempt to parse a typed date
      * @param {Event} event
      */
-    parseTypedDate (event) {
+    parseTypedDate(event) {
       // close calendar if escape or enter are pressed
-      if ([
-        27, // escape
-        13 // enter
-      ].includes(event.keyCode)) {
-        this.input.blur()
+      if (
+        [
+          27, // escape
+          13, // enter
+        ].includes(event.keyCode)
+      ) {
+        this.input.blur();
       }
 
       if (this.typeable) {
-        const typedDate = Date.parse(this.input.value)
-        if (!isNaN(typedDate)) {
-          this.typedDate = this.input.value
-          this.$emit('typedDate', new Date(this.typedDate))
+        const parsedDate = Date.parse(this.getTypedDate(this.input.value));
+        if (!isNaN(parsedDate)) {
+          this.typedDate = this.input.value;
+          this.$emit("typedDate", new Date(this.typedDate));
+          this.$emit("typedDate", new Date(parsedDate));
         }
       }
     },
@@ -129,26 +151,38 @@ export default {
      * nullify the typed date to defer to regular formatting
      * called once the input is blurred
      */
-    inputBlurred () {
-      if (this.typeable && isNaN(Date.parse(this.input.value))) {
-        this.clearDate()
-        this.input.value = null
-        this.typedDate = null
+    inputBlurred() {
+      if (
+        this.typeable &&
+        isNaN(Date.parse(this.getTypedDate(this.input.value)))
+      ) {
+        this.clearDate();
+        this.input.value = null;
+        this.typedDate = null;
       }
 
-      this.$emit('closeCalendar')
+      this.$emit("closeCalendar");
     },
     /**
      * emit a clearDate event
      */
-    clearDate () {
-      this.$emit('clearDate')
-    }
+    clearDate() {
+      this.$emit("clearDate");
+    },
+    /**
+     * format Date with regular or custom function
+     */
+    getTypedDate(input) {
+      let date =
+        typeof this.formatTypedDate === "function"
+          ? this.formatTypedDate(input)
+          : input;
+      return date;
+    },
   },
-  mounted () {
-    this.input = this.$el.querySelector('input')
-  }
-}
+  mounted() {
+    this.input = this.$el.querySelector("input");
+  },
+};
 // eslint-disable-next-line
-;
 </script>
--- test/unit/specs/DateInput/typedDates.spec.js
+++ test/unit/specs/DateInput/typedDates.spec.js
@@ -1,68 +1,90 @@
-import DateInput from '@/components/DateInput.vue'
-import {shallow} from '@vue/test-utils'
-import {en} from '@/locale'
+import DateInput from "@/components/DateInput.vue";
+import { shallow } from "@vue/test-utils";
+import { en } from "@/locale";
 
-describe('DateInput', () => {
-  let wrapper
+describe("DateInput", () => {
+  let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(DateInput, {
       propsData: {
-        format: 'dd MMM yyyy',
+        format: "dd MMM yyyy",
         translation: en,
-        typeable: true
-      }
-    })
-  })
+        typeable: true,
+      },
+    });
+  });
 
-  it('does not format the date when typed', () => {
-    const dateString = '2018-04-24'
-    wrapper.vm.input.value = dateString
-    expect(wrapper.vm.input.value).toEqual(dateString)
+  it("does not format the date when typed", () => {
+    const dateString = "2018-04-24";
+    wrapper.vm.input.value = dateString;
+    expect(wrapper.vm.input.value).toEqual(dateString);
     wrapper.setData({
-      typedDate: dateString
-    })
+      typedDate: dateString,
+    });
     wrapper.setProps({
-      selectedDate: new Date(dateString)
-    })
-    expect(wrapper.vm.typedDate).toEqual(dateString)
-    expect(wrapper.vm.formattedValue).toEqual(dateString)
-  })
+      selectedDate: new Date(dateString),
+    });
+    expect(wrapper.vm.typedDate).toEqual(dateString);
+    expect(wrapper.vm.formattedValue).toEqual(dateString);
+  });
 
-  it('emits the date when typed', () => {
-    const input = wrapper.find('input')
-    wrapper.vm.input.value = '2018-04-24'
-    input.trigger('keyup')
-    expect(wrapper.emitted().typedDate).toBeDefined()
-    expect(wrapper.emitted().typedDate[0][0]).toBeInstanceOf(Date)
-  })
+  it("emits the date when typed", () => {
+    const input = wrapper.find("input");
+    wrapper.vm.input.value = "2018-04-24";
+    input.trigger("keyup");
+    expect(wrapper.emitted().typedDate).toBeDefined();
+    expect(wrapper.emitted().typedDate[0][0]).toBeInstanceOf(Date);
+  });
 
-  it('emits closeCalendar when return is pressed', () => {
-    const input = wrapper.find('input')
-    const blurSpy = jest.spyOn(input.element, 'blur')
-    input.trigger('keyup', {keyCode: 13})
-    expect(blurSpy).toBeCalled()
-  })
+  it("allows custom date format", () => {
+    const dateString = "24-06-2018";
+    wrapper.setProps({
+      selectedDate: new Date(dateString),
+      typeable: true,
+      formatTypedDate: function (dateString) {
+        let result = dateString.split("/");
+        return (
+          result[2] + "-" + result[1] + "-" + result[0] + "T00:00:00+01:00"
+        );
+      },
+    });
+    const input = wrapper.find("input");
+    wrapper.vm.input.value = dateString;
+    expect(wrapper.vm.input.value).toEqual(dateString);
+    input.trigger("keyup");
+    expect(wrapper.emitted().typedDate[0][0].toISOString()).toEqual(
+      "2018-06-24T01:00:00.000Z"
+    );
+    expect(wrapper.vm.formattedValue).toEqual(dateString);
+  });
 
-  it('clears a typed date if it does not parse', () => {
-    const input = wrapper.find('input')
-    wrapper.setData({typedDate: 'not a date'})
-    input.trigger('blur')
-    expect(wrapper.emitted().clearDate).toBeDefined()
-  })
+  it("emits closeCalendar when return is pressed", () => {
+    const input = wrapper.find("input");
+    const blurSpy = jest.spyOn(input.element, "blur");
+    input.trigger("keyup", { keyCode: 13 });
+    expect(blurSpy).toBeCalled();
+  });
 
-  it('doesn\'t emit the date if typeable=false', () => {
+  it("clears a typed date if it does not parse", () => {
+    const input = wrapper.find("input");
+    wrapper.setData({ typedDate: "not a date" });
+    input.trigger("blur");
+    expect(wrapper.emitted().clearDate).toBeDefined();
+  });
+
+  it("doesn't emit the date if typeable=false", () => {
     const wrapper = shallow(DateInput, {
       propsData: {
-        format: 'dd MMM yyyy',
+        format: "dd MMM yyyy",
         translation: en,
-        typeable: false
-      }
-    })
-    const input = wrapper.find('input')
-    wrapper.vm.input.value = '2018-04-24'
-    input.trigger('keydown')
-    input.trigger('keyup')
-    expect(wrapper.emitted().typedDate).not.toBeDefined()
-  })
-})
+        typeable: false,
+      },
+    });
+    const input = wrapper.find("input");
+    wrapper.vm.input.value = "2018-04-24";
+    input.trigger("keydown");
+    input.trigger("keyup");
+    expect(wrapper.emitted().typedDate).not.toBeDefined();
+  });
+});
